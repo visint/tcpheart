c62be387a580dd487107c8a889fcb8771afc3fa0
jiangyibo <jyb99613@gmail.com>
2017-06-12 15:41:43 +0800

    添加文件

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # tcpheart
 gcc hb_client.c tcp_client.c util.c -o client
-
+gcc hb_server.c tcp_server.c util.c -o server
+gcc vrc.c tcp_client.c util.c -o client
+gcc vrs.c tcp_server.c util.c readvrec.c readn.c  -o server
--- hb_client.c
+++ hb_client.c
@@ -1,7 +1,7 @@
 #include "etcp.h"
 #include "heartbeat.h"
 
-int main( int argc, char **argv )
+int main(int argc, char **argv)
 {
 	fd_set allfd;
 	fd_set readfd;
@@ -10,46 +10,46 @@ int main( int argc, char **argv )
 	SOCKET s;
 	int rc;
 	int heartbeats = 0;
-	int cnt = sizeof( msg );
+	int cnt = sizeof(msg);
 
 	INIT();
-	s = tcp_client( argv[ 1 ], argv[ 2 ] );
-	FD_ZERO( &allfd );
-	FD_SET( s, &allfd );
+	s = tcp_client(argv[1], argv[2]);
+	FD_ZERO(&allfd);
+	FD_SET(s, &allfd);
 	tv.tv_sec = T1;
 	tv.tv_usec = 0;
-	for ( ;; )
+	for (;;)
 	{
 		readfd = allfd;
-		rc = select( s + 1, &readfd, NULL, NULL, &tv );
-		if ( rc < 0 )
-			error( 1, errno, "select failure" );
-		if ( rc == 0 )		/* timed out */
+		rc = select(s + 1, &readfd, NULL, NULL, &tv);
+		if (rc < 0)
+			error(1, errno, "select failure");
+		if (rc == 0) /* timed out */
 		{
-			if ( ++heartbeats > 3 )
-				error( 1, 0, "connection dead\n" );
-			error( 0, 0, "sending heartbeat #%d\n", heartbeats );
-			msg.type = htonl( MSG_HEARTBEAT );
-			rc = send( s, ( char * )&msg, sizeof( msg ), 0 );
-			if ( rc < 0 )
-				error( 1, errno, "send failure" );
+			if (++heartbeats > 3)
+				error(1, 0, "connection dead\n");
+			error(0, 0, "sending heartbeat #%d\n", heartbeats);
+			msg.type = htonl(MSG_HEARTBEAT);
+			rc = send(s, (char *)&msg, sizeof(msg), 0);
+			if (rc < 0)
+				error(1, errno, "send failure");
 			tv.tv_sec = T2;
 			continue;
 		}
-		if ( !FD_ISSET( s, &readfd ) )
-			error( 1, 0, "select returned invalid socket\n" );
-		rc = recv( s, ( char * )&msg + sizeof( msg ) - cnt,
-			cnt, 0 );
-		if ( rc == 0 )
-			error( 1, 0, "server terminated\n" );
-		if ( rc < 0 )
-			error( 1, errno, "recv failure" );
+		if (!FD_ISSET(s, &readfd))
+			error(1, 0, "select returned invalid socket\n");
+		rc = recv(s, (char *)&msg + sizeof(msg) - cnt,
+				  cnt, 0);
+		if (rc == 0)
+			error(1, 0, "server terminated\n");
+		if (rc < 0)
+			error(1, errno, "recv failure");
 		heartbeats = 0;
 		tv.tv_sec = T1;
-		cnt -= rc;					/* in-line readn */
-		if ( cnt > 0 )
+		cnt -= rc; /* in-line readn */
+		if (cnt > 0)
 			continue;
-		cnt = sizeof( msg );
+		cnt = sizeof(msg);
 
 		/* process message */
 	}
